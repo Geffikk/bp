9b62bd149d8f7e212519145770fbc7ff264c284d
Maros Geffert <xgeffe00@vutbr.cz>
2021-02-12 09:15:56 +0100

    #1 Initial commit

---
+++ src/main/java/vut/fit/covid/centrum/CovidCentrumApplication.java
@@ -0,0 +1,13 @@
+package vut.fit.covid.centrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidCentrumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidCentrumApplication.class, args);
+	}
+
+}
